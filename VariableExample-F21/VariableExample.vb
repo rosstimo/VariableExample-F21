dcafc5a087753149ed563bd5d47022c7c5749fd5
rosstimo <rosstimo@isu.edu>
2021-09-01 08:43:47 -0600

    add header info

---
+++ VariableExample-F21/VariableExample.vb
@@ -0,0 +1,16 @@
+﻿'Tim Rossiter
+'Fall 2021
+'Variable and Data Type example
+'https://github.com/rosstimo/VariableExample-F21.git
+
+
+
+Module VariableExample
+
+    Sub Main()
+
+
+
+    End Sub
+
+End Module
