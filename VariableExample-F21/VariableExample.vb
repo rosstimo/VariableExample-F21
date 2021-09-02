f7658983388acb248eae1e8dcd91e34a18d918af
rosstimo <rosstimo@isu.edu>
2021-09-02 08:58:58 -0600

    add user input example

--- VariableExample-F21/VariableExample.vb
+++ VariableExample-F21/VariableExample.vb
@@ -7,13 +7,13 @@ Option Strict On
 Option Explicit On
 
 Module VariableExample
-
     Sub Main()
         Dim myNewVariable As String
         Dim firstNumber As Integer
         Dim secondNumber As Integer
         Dim result As Double
         Dim test As Boolean
+        Dim userResponse As String
 
         Const DISCOUNT As Decimal = 0.01@
 
@@ -24,10 +24,10 @@ Module VariableExample
         firstNumber = 5
         secondNumber = 6
         result = firstNumber / secondNumber
+        Console.WriteLine("Enter a number")
+        userResponse = Console.ReadLine()
+        Console.WriteLine(CInt(userResponse) + 5)
 
-        'someStuff = "a thing"
-
-        Console.Read()
+        Console.Beep()
     End Sub
-
 End Module
