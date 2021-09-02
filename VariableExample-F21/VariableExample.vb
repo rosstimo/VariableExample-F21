dbd518448c2e3f0bb2bba0e4477567f1038c9e5c
rosstimo <rosstimo@isu.edu>
2021-09-02 08:34:08 -0600

    add integer variable and constant examples

--- VariableExample-F21/VariableExample.vb
+++ VariableExample-F21/VariableExample.vb
@@ -3,20 +3,31 @@
 'Variable and Data Type example
 'https://github.com/rosstimo/VariableExample-F21.git
 
+Option Strict On
+Option Explicit On
 
 Module VariableExample
 
     Sub Main()
-
         Dim myNewVariable As String
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim result As Double
+        Dim test As Boolean
+
+        Const DISCOUNT As Decimal = 0.01@
 
         myNewVariable = "hello, world!"
-
         myNewVariable = "Good Bye!"
-
         myNewVariable &= " Cruel World"
 
+        firstNumber = 5
+        secondNumber = 6
+        result = firstNumber / secondNumber
 
+        'someStuff = "a thing"
+
+        Console.Read()
     End Sub
 
 End Module
